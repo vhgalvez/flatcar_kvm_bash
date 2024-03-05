1856de6721fc31ba850ea4c00e26d8dfabaf5bbe
Victor Galvez <vhgalvez@gmail.com>
2024-03-05 19:29:26 +0100

    update

--- Documentation.md
+++ Documentation.md
@@ -2,3 +2,4 @@
 sudo dnf install -y butane
 butane --version
 
+chmod +x file_generation_v3.sh
--- file_generation_v3.sh
+++ file_generation_v3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Uso: sh file_generation.sh <nombre_de_la_instancia>
+# Uso: ./file_generation.sh <nombre_de_la_instancia>
 
 set -x
 
@@ -11,7 +11,7 @@ fi
 VM_NAME="$1"
 USER_NAME="core"
 SSH_DIR="$HOME/.ssh"
-SSH_KEY="$SSH_DIR/id_rsa.pub"
+SSH_KEY="$SSH_DIR/id_rsa"
 
 YAML_PATH="$HOME/ign/$VM_NAME-config.yaml"
 IGN_PATH="$HOME/ign/$VM_NAME-config.ign"
@@ -19,12 +19,12 @@ IGN_PATH="$HOME/ign/$VM_NAME-config.ign"
 mkdir -p "$SSH_DIR"
 mkdir -p "$(dirname "$YAML_PATH")"
 
-if [ ! -f "$SSH_KEY" ]; then
-    echo "El archivo $SSH_KEY no existe. Generando una nueva clave SSH..."
+if [ ! -f "$SSH_KEY.pub" ]; then
+    echo "El archivo $SSH_KEY.pub no existe. Generando una nueva clave SSH..."
     ssh-keygen -t rsa -b 2048 -N "" -f "$SSH_KEY"
 fi
 
-SSH_PUB_KEY=$(cat "$SSH_KEY")
+SSH_PUB_KEY=$(cat "$SSH_KEY.pub")
 
 cat > "$YAML_PATH" << EOF
 variant: flatcar
@@ -37,17 +37,17 @@ passwd:
 EOF
 
 if [ ! -f "$YAML_PATH" ]; then
-  echo "Error, el archivo YAML no se generó."
-  exit 1
+    echo "Error, el archivo YAML no se generó."
+    exit 1
 else
-  echo "El archivo YAML se generó correctamente en $YAML_PATH"
+    echo "El archivo YAML se generó correctamente en $YAML_PATH"
 fi
 
 butane --pretty --strict "$YAML_PATH" -o "$IGN_PATH"
 
 if [ ! -f "$IGN_PATH" ]; then
-  echo "Error, el archivo IGN no se generó."
-  exit 1
+    echo "Error, el archivo IGN no se generó."
+    exit 1
 else
-  echo "El archivo IGN se generó correctamente en $IGN_PATH"
+    echo "El archivo IGN se generó correctamente en $IGN_PATH"
 fi
