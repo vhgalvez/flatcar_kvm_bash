25f987cfb385c2fea3a2b1f3cf73378fba5d536a
Victor Galvez <vhgalvez@gmail.com>
2024-03-06 04:22:37 +0100

    Update flatcar_kvm_bash.sh

--- flatcar_kvm_bash.sh
+++ flatcar_kvm_bash.sh
@@ -8,7 +8,7 @@ VM_NAME="flatcar-linux1"
 # Configuraciones de la VM
 RAM=1024
 VCPUS=1
-OS_TYPE="generic"
+OS_VARIANT="generic"
 # Asegúrate de ajustar la ruta de la imagen aquí
 DISK_PATH="/var/lib/libvirt/images/flatcar_image/flatcar_production_qemu_image.img"
 DISK_FORMAT="qcow2"
@@ -23,7 +23,7 @@ virt-install --connect qemu:///system \
              --import \
              --name "$VM_NAME" \
              --ram $RAM --vcpus $VCPUS \
-             --os-type=$OS_TYPE \
+             --os-variant=$OS_VARIANT \
              --disk path=$DISK_PATH,format=$DISK_FORMAT,bus=$DISK_BUS \
              --vnc --noautoconsole \
              --qemu-commandline="-fw_cfg name=opt/org.flatcar-linux/config,file=$CONFIG_FILE"
